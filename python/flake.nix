b00a000c0b97015e17f54b89f969e7891530f783
Adrian Hesketh <inbox@adrianhesketh.com>
2025-11-03 15:50:08 +0000

    feat: use uv to manage Python deps

--- python/flake.nix
+++ python/flake.nix
@@ -1,13 +1,24 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    xc = {
-      url = "github:joerdav/xc";
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, xc }:
+  outputs = { nixpkgs, pyproject-nix, uv2nix, pyproject-build-systems, ... }:
     let
       allSystems = [
         "x86_64-linux" # 64-bit Intel/AMD Linux
@@ -16,55 +27,89 @@
         "aarch64-darwin" # 64-bit ARM macOS
       ];
 
+      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+
+      overlay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel";
+      };
+
+      editableOverlay = workspace.mkEditablePyprojectOverlay {
+        root = "$REPO_ROOT";
+      };
+
+      pyprojectOverrides = final: prev: {
+        # These projects don't have setuptools declared as a build system requirement,
+        # so we need to add it manually.
+        antlr4-python3-runtime = prev.antlr4-python3-runtime.overrideAttrs (old: {
+          buildInputs = (old.buildInputs or [ ]) ++ final.resolveBuildSystem ({ setuptools = [ ]; });
+        });
+        dsnparse = prev.dsnparse.overrideAttrs (old: {
+          buildInputs = (old.buildInputs or [ ]) ++ final.resolveBuildSystem ({ setuptools = [ ]; });
+        });
+        # Some projects are complex to build. If there's a version in nixpkgs, we can
+        # use that instead.
+        mysql-connector-python = final.pkgs.python312Packages.mysql-connector;
+        pip = final.pkgs.python312Packages.pip;
+        python-lsp-server = final.pkgs.python312Packages.python-lsp-server;
+        # If the package isn't in nixpkgs at all, then you'll have to package it.
+        # See https://github.com/NixOS/nixpkgs/blob/nixos-24.11/pkgs/development/python-modules/streamlit/default.nix
+        # as an example.
+      };
+
       forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system:
         let
           pkgs = import nixpkgs {
             inherit system;
-            overlays = [
-              (self: super: {
-                # Add xc to nixpkgs.
-                xc = xc.outputs.packages.${system}.xc;
-              })
-            ];
           };
-          # Set the Python version for all packages.
+
           python = pkgs.python312;
+          pythonPkgs = (pkgs.callPackage pyproject-nix.build.packages {
+            inherit python;
+          }).overrideScope
+            (
+              nixpkgs.lib.composeManyExtensions [
+                pyproject-build-systems.overlays.default
+                overlay
+                pyprojectOverrides
+              ]
+            );
+          pythonPkgsEditable = (pkgs.callPackage pyproject-nix.build.packages {
+            inherit python;
+          }).overrideScope
+            (
+              nixpkgs.lib.composeManyExtensions [
+                pyproject-build-systems.overlays.wheel
+                editableOverlay
+                pyprojectOverrides
+              ]
+            );
+
+          pythonEnv = (pythonPkgs.mkVirtualEnv "app-env" workspace.deps.all);
+          app = (pkgs.stdenv.mkDerivation {
+            name = "app";
+            src = ./.;
+
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+            buildInputs = [ pythonEnv ];
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp -r ./ $out/bin/${name}-script
+              makeWrapper ${pythonEnv}/bin/python $out/bin/${name} \
+                --set PYTHONPATH "$out/bin/${name}-script/src" \
+                --add-flags $out/bin/${name}-script/src/app/main.py
+            '';
+          });
         in
         f {
-          inherit system pkgs python;
+          inherit system pkgs pythonPkgs pythonPkgsEditable pythonEnv app;
         }
       );
 
-      # If the version in Nixpkgs is old, you can override the src.
-      overriddenStreamlit = { pkgs, sl }: sl.overridePythonAttrs (old: rec {
-        version = "1.44.1";
-        src = pkgs.fetchPypi {
-          inherit version;
-          pname = "streamlit";
-          hash = "sha256-xpFO1tW3aHC0YVEEdoBts3DzZCWuDmZU0ifJiCiBmNM="; # Set this to "", and nix will error after the download, giving the hash.
-        };
-      });
-
-      # If the package isn't in nixpkgs at all, then you'll have to package it.
-      # See https://github.com/NixOS/nixpkgs/blob/nixos-24.11/pkgs/development/python-modules/streamlit/default.nix
-      # as an example.
-
-      pythonDeps = pkgs: ps: [
-        #(overriddenStreamlit { inherit pkgs; sl = ps.streamlit; })
-        ps.streamlit
-        ps.authlib # Required for streamlit auth.
-        ps.uvicorn
-        ps.fastapi
+      extraPythonDeps = pkgs: ps: [
         ps.python-lsp-server
-        ps.pandas
-        ps.watchdog
+        ps.pip
       ];
-
-      app = { name, pkgs, python, ... }: python.pkgs.buildPythonApplication {
-        inherit name;
-        src = ./.;
-        propagatedBuildInputs = (pythonDeps pkgs python.pkgs);
-      };
 
       # Build Docker containers.
       dockerUser = pkgs: pkgs.runCommand "user" { } ''
@@ -73,7 +118,7 @@
         echo "user:x:1000:" > $out/etc/group
         echo "user:!:1::::::" > $out/etc/shadow
       '';
-      dockerImage = { name, system, pkgs, python }: pkgs.dockerTools.buildImage {
+      dockerImage = { name, pkgs, app, ... }: pkgs.dockerTools.buildImage {
         name = name;
         tag = "latest";
 
@@ -85,7 +130,7 @@
           pkgs.cacert
           pkgs.dockerTools.caCertificates
           (dockerUser pkgs)
-          (app { inherit name system pkgs python; })
+          app
         ];
         config = {
           Cmd = [ name ];
@@ -100,35 +145,33 @@
       # Development tools used.
       devTools = pkgs: [
         pkgs.crane
-        pkgs.gh
-        pkgs.git
+        pkgs.uv
         pkgs.xc
       ];
 
       name = "app";
     in
     {
-      devShells = forAllSystems ({ system, pkgs, python, ... }: {
+      devShells = forAllSystems ({ pkgs, pythonPkgsEditable, pythonEnv, ... }: {
         default = pkgs.mkShell {
-          packages =
-            (devTools pkgs) ++
-            [
-              (python.withPackages (pythonDeps pkgs))
-            ];
+          packages = [ pythonEnv ] ++ (devTools pkgs) ++ (extraPythonDeps pkgs pythonPkgsEditable);
+          env = {
+            UV_NO_SYNC = "1";
+            UV_PYTHON = "${pythonEnv}/bin/python";
+            UV_PYTHON_DOWNLOADS = "never";
+          };
+          shellHook = ''
+            unset PYTHONPATH
+            export REPO_ROOT=$(git rev-parse --show-toplevel)
+            export PYTHONPATH=$REPO_ROOT/src
+          '';
         };
       });
       packages = forAllSystems
-        ({ system, pkgs, python, ... }: {
-          default = app {
-            name = name;
-            pkgs = pkgs;
-            python = python;
-          };
+        ({ system, pkgs, app, ... }: {
+          default = app;
           docker-image = dockerImage {
-            name = name;
-            system = system;
-            pkgs = pkgs;
-            python = python;
+            inherit name system pkgs app;
           };
         });
     };
